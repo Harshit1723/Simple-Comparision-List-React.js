2e0982f41139304248e405297962549772d5d91e
Harshit <harshitsingh172023@gmail.com>
2023-07-08 12:18:13 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
 
-function App() {
+export default function App(){
+  const[listA, setListA] =useState([]);
+  const[listB, setListB]= useState([]);
+  const[differences, setDifferences]=useState(null);
+
+  const handleListAChange = (event) => {
+    const newListA=event.target.value.split('\n');
+    setListA(newListA);
+  }
+
+  const handleListBChange = (event) => {
+    const newListB = event.target.value.split('\n');
+    setListB(newListB);
+  };
+
+  function computeDifferences(){
+
+    const itemsOnlyinA=listA.filter((item) => !listB.includes(item));
+    const itemsOnlyinB=listB.filter((item) => !listA.includes(item));
+
+    const itemsinBoth= listA.filter((item) => listB.includes(item));
+    const combinedItems=[...new Set([...listA,...listB])];
+
+    const differences = {
+      itemsOnlyinA,
+      itemsOnlyinB,
+      itemsinBoth,
+      combinedItems
+    };
+
+    setDifferences(differences);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <textarea className="textarea" placeholder="List A"value={listA.join('\n')} onChange={handleListAChange} />
+
+      <br />
+      
+      
+
+      <textarea className="textarea" placeholder='List B' value={listB.join('\n')} onChange={handleListBChange} />
+
+      <button className="button" onClick={computeDifferences}>Compute</button>
+    
+      {differences && (
+        <div  className="result">
+          <p>Items only in A : {differences.itemsOnlyinA.join(',')}</p>
+          <p>Items only in B : {differences.itemsOnlyinB.join(',')}</p>
+          <p>Items in Both A and B : {differences.itemsinBoth.join(',')}</p>
+          <p>Combines items of A and B :{differences.combinedItems.join(',')}</p>
+        </div>
+      )}
+    
     </div>
   );
 }
-
-export default App;
